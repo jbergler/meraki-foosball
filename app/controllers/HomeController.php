7ea85fc76696098e007c7f541ea3d1d56d05feab
Jonas Bergler <jonas@bergler.name>
2014-04-01 21:49:15 +0000

    Only show players who've played in rankings

--- app/controllers/HomeController.php
+++ app/controllers/HomeController.php
@@ -11,15 +11,15 @@ class HomeController extends \BaseController {
 	 */
 	public function index()
 	{
-		$singles = User::query()->orderBy('rating_singles', 'desc')->orderBy('name', 'asc')->limit(10)->get();
-		$doubles = User::query()->orderBy('rating_doubles', 'desc')->orderBy('name', 'asc')->limit(10)->get();
+		$singles = User::query()->where('count_singles', '>', 0)->orderBy('rating_singles', 'desc')->orderBy('name', 'asc')->limit(20)->get();
+		$doubles = User::query()->where('count_doubles', '>', 0)->orderBy('rating_doubles', 'desc')->orderBy('name', 'asc')->limit(20)->get();
 
-		$games = Game::query()->orderBy('created_at', 'desc')->limit(10)->get();
+		$games = Game::query()->orderBy('created_at', 'desc')->limit(200)->get();
 
 		return View::make('users')
 					->with('games', $games)
 					->with('singles', $singles)
-					->with('doubles', $doubles);	
+					->with('doubles', $doubles);
 	}
 
 }
